32745c9c3a522b24c001c471a2c44d66770cf13c
puerarongo <akhardin96@gmail.com>
2022-09-07 16:11:16 +0300

    add middleware

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,12 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import { persistReducer, persistStore } from 'redux-persist'
 import storage from 'redux-persist/lib/storage';
 
 import reducers from "./reducer/reducers";
+
+const middleware = getDefaultMiddleware({
+    serializableCheck: false,
+});
 
 const persistConfig = {
     key: 'root',
@@ -11,7 +15,8 @@ const persistConfig = {
 
 
 export const store = configureStore({
-    reducer: persistReducer(persistConfig, reducers)
+    reducer: persistReducer(persistConfig, reducers),
+    middleware
 });
 
 export const persistor = persistStore(store);
